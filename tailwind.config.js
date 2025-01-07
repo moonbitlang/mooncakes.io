c99668404bac4bf4bdd8df241e48cf13da572efd
东灯 <me@lampese.com>
2025-01-07 14:58:44 +0800

    fix: menu render error

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js,mbt}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"Poppins"', "sans-serif"],
+      fira: ['"Fira Sans"'],
+    },
+    extend: {
+      animation: {
+        "logo-spin": "spin 10s linear infinite",
+      },
+      dropShadow: {
+        logo: "0 0 5rem #F3AD61",
+      },
+    },
+    extend: {
+      fontSize: {
+        tiny: "0.7rem",
+      },
+      colors: {
+        moonbit: "#cf4f89",
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar")],
-}
-
+};

acad3e69a3a6d9ce9e8e68a7d7a9769cd1cd9716
Yorkin <cyb_yorkin@outlook.com>
2025-06-17 13:39:13 +0800

    refactor: use rabbit-tea-vite plugin, upgrade to tailwindcss4

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,43 +1,8 @@
 import { defineConfig } from 'vite'
-import chokidar from 'chokidar'
-import cp from 'node:child_process'
-import fs from 'node:fs'
-import path from 'node:path'
-
-/**
- * @type {import('vite').Plugin}
- */
-var moonProcess = null
-const moonbitBuilder = {
-    buildStart() {
-        fs.rmSync('target/web', { recursive: true, force: true })
-        fs.mkdirSync('target/web', { recursive: true })
-
-        moonProcess = cp.spawn('moon', ['build', '--target', 'js', '--watch'], { stdio: 'ignore' });
-
-        function build() {
-            fs.copyFileSync('target/js/release/build/main/main.js', 'target/web/main.js')
-            fs.cpSync('web', 'target/web', { recursive: true })
-            cp.execSync('tailwindcss -i web/styles.css -o target/web/styles.css')
-        }
-
-        build()
-
-        chokidar.watch([
-            'target/js/release/build/main/main.js',
-            'src/main/index.html',
-            'src/main/styles.css'],
-            { ignoreInitial: true }).on('all', (event, path) => {
-                build()
-            })
-    },
-    closeBundle() {
-        if (moonProcess) {
-            moonProcess.kill()
-            console.log('moon process exited.')
-        }
-    }
-}
+import rabbitTEA from 'rabbit-tea-vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import fs from 'fs'
 
 const testAssetsServer = {
     name: 'static',
@@ -48,8 +13,10 @@ const testAssetsServer = {
                 try {
                     const content = fs.readFileSync(p)
                     res.write(content)
+                    console.log('Request succeed:', req.url)
                 } catch {
                     res.statusCode = 404
+                    console.log('Request failed:', req.url)
                 }
                 res.end()
             } else {
@@ -60,6 +27,11 @@ const testAssetsServer = {
 }
 
 export default defineConfig({
-    root: 'target/web',
-    plugins: [moonbitBuilder, testAssetsServer],
+    root: 'src',
+    publicDir: '../public',
+    plugins: [
+        rabbitTEA(),
+        tailwindcss(),
+        testAssetsServer
+    ],
 })

0eb6912aa1a3f03e1b697ef0a2841dc5633d88f4
Yorkin <cyb_yorkin@outlook.com>
2025-09-19 18:48:52 +0800

    refactor: use api/v0/module in home page instead of generated files

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,37 +5,54 @@ import path from 'path'
 import fs from 'fs'
 
 const testAssetsServer = {
-    name: 'static',
-    configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-            if (req.url.startsWith('/assets')) {
-                const p = path.join('test', req.url)
-                try {
-                    const content = fs.readFileSync(p)
-                    res.write(content)
-                    console.log('Request succeed:', req.url)
-                } catch {
-                    res.statusCode = 404
-                    console.log('Request failed:', req.url)
-                }
-                res.end()
-            } else {
-                next()
-            }
-        })
-    }
+  name: 'static',
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url.startsWith('/assets')) {
+        const p = path.join('test', req.url)
+        try {
+          const content = fs.readFileSync(p)
+          res.write(content)
+          console.log('Request succeed:', req.url)
+        } catch {
+          res.statusCode = 404
+          console.log('Request failed:', req.url)
+        }
+        res.end()
+      } else {
+        next()
+      }
+    })
+  }
 }
 
 export default defineConfig({
-    root: 'src',
-    publicDir: '../public',
-    build: {
-        outDir: '../dist',
-        assetsDir: ''
-    },
-    plugins: [
-        rabbitTEA(),
-        tailwindcss(),
-        testAssetsServer
-    ],
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    assetsDir: ''
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://mooncakes.io',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('Proxy error:', err)
+          })
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Proxying:', req.method, req.url)
+          })
+        }
+      }
+    }
+  },
+  plugins: [
+    rabbitTEA(),
+    tailwindcss(),
+    testAssetsServer
+  ],
 })
